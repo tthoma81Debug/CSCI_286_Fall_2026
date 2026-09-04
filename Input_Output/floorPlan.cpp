b0736856a2c253934ac3058d6785c28d7752da6e
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-09-04 09:55:06 -0400

    boolAndIntDivision

--- Input_Output/floorPlan.cpp
+++ Input_Output/floorPlan.cpp
@@ -15,6 +15,18 @@ int main()
     int numberToDivide = 8;
     int answer;
 
+    //bool hasPurchased = true;
+    //cout << "have you purchased flooring yet? Enter 1 for yes, and 0 for no \n";
+    //cin >> answer;
+
+    int totalFloorTilesPurchased = 100;
+    int totalFloorTilesAvailable = 800;
+
+    int totalAvailableTilesUsed = totalFloorTilesPurchased / totalFloorTilesAvailable;
+
+    cout << "total available tiles uses (as percent): " << totalAvailableTilesUsed; //note that this is actually 0 because it is integer division.
+
+
     cout << "Lets find out how many square feet of flooring you need. How long is the room? \n";
     cin >> length;
 
