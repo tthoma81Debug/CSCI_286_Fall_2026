b3cc05e8af3e85f0192a3138dd594ccb7d8710e8
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-09-11 09:27:06 -0400

    in progress

--- SpacecraftCountdownClock/ClockExample.cpp
+++ SpacecraftCountdownClock/ClockExample.cpp
@@ -6,17 +6,21 @@ int main()
 {
     int userSuppliedTotalSeconds;
     int minutes;
+    int hours;
     int leftoverSeconds;
     string minutesString;
     string secondsString;
     string outputString;
+    string hoursString;
 
     cout << "Please enter the countdown timer amount in seconds. \n";
     cin >> userSuppliedTotalSeconds;
 
     minutes = userSuppliedTotalSeconds / 60;
+    hours = (userSuppliedTotalSeconds / 60) / 60;
     leftoverSeconds = userSuppliedTotalSeconds % 60;
 
+    hoursString = to_string(hours);
     minutesString = to_string(minutes);
     secondsString = to_string(leftoverSeconds);
 
@@ -26,6 +30,11 @@ int main()
     outputString = minutesString + secondsString;
 
     cout << outputString;
+
+    //cout << endl;
+    cout << "\n";
+
+    cout << "hours is " + hoursString;
 
     return 0;
 }
