0f83ce182a1b8762d576213bfbaa79bf1bc7eda5
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-09-14 09:43:50 -0400

    problemTenPreDebug

--- problemTen/problemTen.cpp
+++ problemTen/problemTen.cpp
@@ -31,8 +31,11 @@ int main()
 
     cout << "Starting Float Value " << floatAltitudeCopy << "\n";
     cout << "Final Float Value " << floatAltitude << "\n";
+    cout << "Starting Double Value " << doubleAltitudeCopy << "\n";
+    cout << "Final Double Value " << doubleAltitude << "\n";
     cout << "Change measured by the float" << changeFloat << "\n";
     cout << "Change measured by the double" << changeDouble << "\n";
+    cout << "Expected Change " << "1.0" << "\n";
 
 
 
