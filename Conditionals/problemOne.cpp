aac48a123104f351d761e4913faef61592d89f9b
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-09-18 09:24:29 -0400

    working

--- Conditionals/problemOne.cpp
+++ Conditionals/problemOne.cpp
@@ -1,10 +1,17 @@
-#include "greeting.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
- 
+    double completedServiceHours;
+    string sayThisString = "Starting Value";
+
+    cout << "Please enter the amount of completed service hours \n";
+    cin >> completedServiceHours;
+
+    cout << sayThisString;
+
+
     return 0;
 }
