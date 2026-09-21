d94cf9f1adb28a96d1cc4afad811db8b39fd9f5b
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-09-21 09:49:53 -0400

    finished problem 9

--- Conditionals/problemNine.cpp
+++ Conditionals/problemNine.cpp
@@ -21,9 +21,33 @@ int main()
     cout <<"The final_random number we generated is " << final_random << "\n";
 
     */
-
+    string outputString;
     int random_number = (rand() % 51) - 10; //will give 0 - 50 and then becomes -10 through 40
     //int rand_step_2 = initial_rand + 1; //will give 1 - 40
+
+    cout <<"random number is " << random_number << "\n";
+
+    if(random_number < 0)
+    {
+        outputString = "Below Freezing";
+    }
+    else if(random_number >= 0 && random_number <= 15)
+    {
+        outputString = "Cold";
+    }
+    else if(random_number > 15 && random_number <= 30)
+    {
+        outputString = "Mild";
+    }
+    else if(random_number > 30)
+    {
+        outputString = "Hot";
+    }
+    else
+    {
+        outputString = "this should never happen. if you see it. something went wrong";
+    }
+    cout << outputString;
 
 
     return 0;
