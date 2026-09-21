e38e5f0199e28cb094db2774c922f4e31695e6e3
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-09-21 09:26:31 -0400

    code

--- Conditionals/problemNine.cpp
+++ Conditionals/problemNine.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-   
+    srand(time(nullptr));
+
+    int raw_random;
+    int random_one;
+    
+    raw_random = rand();
+    random_one = raw_random % 10;
+
+    cout <<"The raw_random number we generated is " << raw_random << "\n";
+    cout <<"The random_one number we generated is " << random_one << "\n";
 
     return 0;
 }
