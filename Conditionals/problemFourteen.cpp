9631937af22b8c77e05f23fdbfe646523214e6d2
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-09-23 09:19:07 -0400

    building

--- Conditionals/problemFourteen.cpp
+++ Conditionals/problemFourteen.cpp
@@ -5,6 +5,17 @@ using namespace std;
 
 int main()
 {
+    int a_variable;
+    int b_variable;
+    int c_variable;
+    cout <<"Enter the valu of 'a'in the quadratic equation \n";
+    cin >> a_variable;
+    cout <<"Enter the valu of 'b'in the quadratic equation \n";
+    cin >> b_variable;
+    cout <<"Enter the valu of 'c'in the quadratic equation \n";
+    cin >> c_variable;
     
+    cout <<"test" << b_variable << " " << c_variable;
+
     return 0;
 }
